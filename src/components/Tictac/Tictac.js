278175da2af5375047410bff89faa9118a8f9bb6
RA-1124 <alison.rangelperez@gmail.com>
2023-10-14 15:36:12 -0600

    cuadrados 9 del tic tac

---
+++ src/components/Tictac/Tictac.js
@@ -0,0 +1,24 @@
+import "./styles.css"
+
+export default function Square() {
+    return (
+    <>
+    <div>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    </div>
+    <div>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    </div>
+    <div>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    </div>
+
+    </>
+    )
+  }
