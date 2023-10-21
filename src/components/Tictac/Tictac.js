02fac2369c0c86a8ede5428c6b59f04e578de530
ZoiloMendoza <zoilomm@hotmail.com>
2023-10-20 21:44:15 -0600

    up

--- src/components/Tictac/Tictac.js
+++ src/components/Tictac/Tictac.js
@@ -4,36 +4,39 @@ import { useState } from "react"
 
 
  const Square =({valorAlClick ,dato}) => {
-    console.log(dato)
+    console.log(dato, 'hijo')
+
     return(
         <button onClick={valorAlClick} className="square"> {dato} </button>
     )
  }
 
 export default function Board() {
-    const [values, setvalues] = useState([null, null, null, null,null, null, null, null, null])
-    function handleClick() {
-        let prueba = values
-        setvalues(prueba[1]="O");
+    const [values, setValues] = useState([null, null, null, null,null, null, null, null, null])
+    
+    function handleClick(i) {
+        const arrayNuevo = values.slice()
+        arrayNuevo[i] = "X"
+        setValues(arrayNuevo);
      }
     
     return (
     <>
     <div className="row">
-    <Square valorAlClick={handleClick} dato={values[0]}/>
-    <Square valorAlClick={handleClick} dato={values[1]} />
-    <Square valorAlClick={handleClick} dato={values[2]} />
+    <Square valorAlClick={() => handleClick(0)} dato={values[0]}/>
+    <Square valorAlClick={() => handleClick(1)} dato={values[1]} />
+    <Square valorAlClick={() => handleClick(2)} dato={values[2]} />
     
     </div>
     <div  className="row">
-    <Square valorAlClick={handleClick} dato={values[3]} />
-    <Square  valorAlClick={handleClick} dato={values[4]}/>
-    <Square  valorAlClick={handleClick} dato={values[5]}/>
+    <Square valorAlClick={() => handleClick(3)} dato={values[3]} />
+    <Square  valorAlClick={() => handleClick(4)} dato={values[4]}/>
+    <Square  valorAlClick={() => handleClick(5)} dato={values[5]}/>
     </div>
     <div  className="row">
-    <Square  valorAlClick={handleClick} dato={values[6]}/>
-    <Square valorAlClick={handleClick} dato={values[7]} />
-    <Square  valorAlClick={handleClick} dato={values[8]}/>
+    <Square  valorAlClick={() => handleClick(6)} dato={values[6]}/>
+    <Square valorAlClick={() => handleClick(7)} dato={values[7]} />
+    <Square  valorAlClick={() => handleClick(8)} dato={values[8]}/>
     </div>
 
     </>
