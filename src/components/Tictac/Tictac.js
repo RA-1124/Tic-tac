dde6e1983e036db9de3c9bf05a0e0157e9cd885b
RA-1124 <alison.rangelperez@gmail.com>
2023-10-23 20:03:44 -0600

    agregar el texto al ganador

--- src/components/Tictac/Tictac.js
+++ src/components/Tictac/Tictac.js
@@ -1,83 +1,93 @@
-import "./styles.css"
-import { useState } from "react"
+import "./styles.css";
+import { useState } from "react";
 
- const Square =({valorAlClick ,dato}) => {
-    //valor al Click es uan funcion y dato es object hasta que se da click y se convierte a string 
-    //console.log(typeof dato, 'tipo de dato')
-   // console.log(typeof valorAlClick, "tipo de valorAlClick")
-    return(
-        <button onClick={valorAlClick} className="square"> {dato} </button>
-    )
- }
+const Square = ({ valorAlClick, dato }) => {
+  //valor al Click es uan funcion y dato es object hasta que se da click y se convierte a string
+  //console.log(typeof dato, 'tipo de dato')
+  // console.log(typeof valorAlClick, "tipo de valorAlClick")
+  return (
+    <button onClick={valorAlClick} className="square">
+      {" "}
+      {dato}{" "}
+    </button>
+  );
+};
 
 export default function Board() {
-    const [values, setValues] = useState([null, null, null, null,null, null, null, null, null])
-    const [click, setClick] = useState(true)
-    
-    function handleClick(i) {
-        if ( values[i] || calculateWinner(values)) {
+  const [values, setValues] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+  const [click, setClick] = useState(true);
 
-            //si el value es igual a null (false) por lo tanto no entra en la condicion y sigue con lo demas, 
-            // si el value es true entonces entra al if y e lreturn hace que salga de la funcion 
-            return
-        } 
-     
-        const arrayNuevo = [...values]
-        //le pasamos un parametro que es indice para que cambie solo el de esa posicion 
-        if (click ) {
-            arrayNuevo[i] = "X"
-        } else {
-            arrayNuevo[i] = "O"
-        }
-       
-        //console.log('renderizandome')
-        setValues(arrayNuevo);
-        setClick(!click )
-     }
-     if (calculateWinner(values)) {
-        console.log("alguien gano!")
+  function handleClick(i) {
+    if (values[i] || calculateWinner(values)) {
+      //si el value es igual a null (false) por lo tanto no entra en la condicion y sigue con lo demas,
+      // si el value es true entonces entra al if y e lreturn hace que salga de la funcion
+      return;
+    }
+
+    const arrayNuevo = [...values];
+    //le pasamos un parametro que es indice para que cambie solo el de esa posicion
+    if (click) {
+      arrayNuevo[i] = "X";
     } else {
-        console.log("el juego continua ")
+      arrayNuevo[i] = "O";
     }
-    return (
+
+    //console.log('renderizandome')
+    setValues(arrayNuevo);
+    setClick(!click);
+  }
+  if (calculateWinner(values)) {
+    const winner = calculateWinner(values)
+    console.log(winner);
+    return <h2> Has ganado {winner}!!</h2>;
+  }
+  return (
     <>
-    <div className="row">
-    <Square valorAlClick={() => handleClick(0)} dato={values[0]}/>
-    <Square valorAlClick={() => handleClick(1)} dato={values[1]} />
-    <Square valorAlClick={() => handleClick(2)} dato={values[2]} />
-    
-    </div>
-    <div  className="row">
-    <Square valorAlClick={() => handleClick(3)} dato={values[3]} />
-    <Square  valorAlClick={() => handleClick(4)} dato={values[4]}/>
-    <Square  valorAlClick={() => handleClick(5)} dato={values[5]}/>
-    </div>
-    <div  className="row">
-    <Square  valorAlClick={() => handleClick(6)} dato={values[6]}/>
-    <Square valorAlClick={() => handleClick(7)} dato={values[7]} />
-    <Square  valorAlClick={() => handleClick(8)} dato={values[8]}/>
-    </div>
-
+      <div className="row">
+        <Square valorAlClick={() => handleClick(0)} dato={values[0]} />
+        <Square valorAlClick={() => handleClick(1)} dato={values[1]} />
+        <Square valorAlClick={() => handleClick(2)} dato={values[2]} />
+      </div>
+      <div className="row">
+        <Square valorAlClick={() => handleClick(3)} dato={values[3]} />
+        <Square valorAlClick={() => handleClick(4)} dato={values[4]} />
+        <Square valorAlClick={() => handleClick(5)} dato={values[5]} />
+      </div>
+      <div className="row">
+        <Square valorAlClick={() => handleClick(6)} dato={values[6]} />
+        <Square valorAlClick={() => handleClick(7)} dato={values[7]} />
+        <Square valorAlClick={() => handleClick(8)} dato={values[8]} />
+      </div>
     </>
-    )
-  }
+  );
+}
 
-  function calculateWinner(values) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (values[a] === values[b] && values[a] === values[c]) {
-        return values[a];
-      }
+function calculateWinner(values) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (values[a] === values[b] && values[a] === values[c]) {
+      return values[a];
     }
-    return null;
   }
+  return null;
+}
